5e1d0d437c637063744f174bb616018525d5aaba
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-08-30 21:30:26 +0300

    add

--- migrations/changelog/clusters/analytics_cluster/analytics/sql/20260801120100_users_add_lifetime_value.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset ${author_name}:users_add_lifetime_value
---comment: Touches a table Liquibase did not create - the case this repo exists to test.
-alter table ${analytics}users on cluster analytics_cluster
-    add column if not exists lifetime_value Decimal(18, 4) default 0;
---rollback alter table ${analytics}users on cluster analytics_cluster drop column if exists lifetime_value;
